5548934b9b5fde872301c77596049495ee8326c9
Thomas M Krystyan <217226122+thomas-mkrystyan_epam@users.noreply.github.com>
2025-07-23 17:40:04 +0200

    Include 2/3 examples how to communicate with chat client

--- SemanticKernel/Program.cs
+++ SemanticKernel/Program.cs
@@ -2,6 +2,7 @@
 using Azure.AI.OpenAI;
 using Microsoft.Extensions.Configuration;
 using OpenAI;
+using OpenAI.Chat;
 using SemanticKernel.Extensions;
 
 namespace SemanticKernel
@@ -25,6 +26,36 @@ namespace SemanticKernel
             // Deployments
             var chatGpt4o = azureClient.GetChatClient("gpt-4o");
             var imageDallE3 = azureClient.GetImageClient("dall-e-3");
+
+            // Example #1:
+            // var result = chatGpt4o.CompleteChat("How are you?");
+
+            // Example #2:
+            var requestOptions = new ChatCompletionOptions
+            {
+                MaxOutputTokenCount = 4096,
+                Temperature = 1.0f,
+                TopP = 1.0f
+            };
+
+            var messages = new List<ChatMessage>
+            {
+                new SystemChatMessage("You are a tourist guide giving a short and concise tips in a bullet-list type of answers"),
+                new UserChatMessage("I am going to Madrid for a couple of years. What should I see there?")
+            };
+
+            var result = chatGpt4o.CompleteChat(messages, requestOptions);
+
+            // Statistics
+            Console.WriteLine($"Input token count: {result.Value.Usage.InputTokenCount}");
+            Console.WriteLine($"Output token count: {result.Value.Usage.OutputTokenCount}");
+            Console.WriteLine($"Total token count: {result.Value.Usage.TotalTokenCount}");
+
+            // Answer
+            Console.WriteLine($"{Environment.NewLine}Answer ({result.Value.Content[0].Kind}):" +
+                              $"{Environment.NewLine}{result.Value.Content[0].Text}");
+            
+            Console.ReadKey();
         }
 
         private static IConfigurationRoot ConfigureApp()
