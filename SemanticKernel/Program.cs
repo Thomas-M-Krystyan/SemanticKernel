21d31ebd741a5cad853460c5465a658d3b488291
Thomas M Krystyan <217226122+thomas-mkrystyan_epam@users.noreply.github.com>
2025-07-23 13:30:09 +0200

    Add project files.

---
+++ SemanticKernel/Program.cs
@@ -0,0 +1,24 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using OpenAI.Chat;
+
+namespace SemanticKernel
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var endpoint = new Uri("https://thoma-mdepts9j-swedencentral.cognitiveservices.azure.com/");
+            var model = "gpt-4o";
+            var deploymentName = "gpt-4o";
+            var apiKey = "<your-api-key>";
+
+            AzureOpenAIClient azureClient = new(
+                endpoint,
+                new AzureKeyCredential(apiKey));
+            ChatClient chatClient = azureClient.GetChatClient(deploymentName);
+        }
+    }
+}
